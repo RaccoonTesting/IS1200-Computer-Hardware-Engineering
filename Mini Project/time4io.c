e1853096f4e3d02e287f2d272df3418a6b3af900
Schubertski <johannaschubert97@gmail.com>
2021-03-30 09:48:49 +0200

    Add files via upload

---
+++ Mini Project/time4io.c
@@ -0,0 +1,18 @@
+//
+//  time4io.c
+//  
+//
+//  Created by Malin Hall on 2020-02-11.
+//
+
+#include <stdint.h>
+#include <pic32mx.h>
+#include "mipslab.h"
+
+int getsw (void){
+    return ((PORTD & 0xf00) >> 8);
+}
+
+int getbtns (void){
+    return ((PORTD & 0xe0) >> 5);
+}
